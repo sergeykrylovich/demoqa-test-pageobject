b5a84a09a0d03ef4022fef24232e4fd9362a2393
sergeikrylovich <sergeybumbum@gmail.com>
2023-05-21 21:59:34 +0300

    Add 6 methods with PageObj

---
+++ src/test/java/tests/RegFormWithPageObj.java
@@ -0,0 +1,80 @@
+package tests;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import pages.RegistrationPage;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class RegFormWithPageObj {
+    RegistrationPage page = new RegistrationPage();
+    @BeforeAll
+    static void beforeAll() {
+
+        browser = "chrome";
+        baseUrl = "https://demoqa.com";
+        browserSize = "1920x1080";
+    }
+
+    @Test
+    void testForm() {
+
+        page.openPage()
+                .setFirstName("John")
+                .setLastName("Malkovich")
+                .setUserEmail("JohnMalkovich@gmail.com")
+                .setPhoneNumber("4441122345")
+                .setGender("Female");
+
+        //Select all data in 'Date of Birth' field
+        $("#dateOfBirthInput").sendKeys(Keys.chord(Keys.CONTROL, "a"));
+        //append a birth date to the 'Date of Birth' field and press enter
+        $("#dateOfBirthInput").append("02 FEB 1990").pressEnter();
+        //add data to the 'Subjects' field
+        $("#subjectsInput").setValue("Maths").pressEnter().setValue("Physics").pressEnter();
+        //select all checkboxes of the 'Hobbies' field
+        $("[for='hobbies-checkbox-1']").click(); // Sports
+        $("[for='hobbies-checkbox-2']").click(); // Reading
+        $("[for='hobbies-checkbox-3']").click(); // Music
+
+        //uploading file for picture
+        //File file = new File("src/test/resources/img/img.jpg");
+        //$("#uploadPicture").uploadFile(file);
+        $("#uploadPicture").uploadFromClasspath("img/img.jpg");
+
+        $("#currentAddress").setValue("17 Dean St, Brooklyn, NY 11201, USA"); // Set Current Address
+
+        //Select City and State fields
+        $("#state").click();
+        $("#state").$(byText("NCR")).click();
+        $("#city").click();
+        $("#city").$(byText("Delhi")).click();
+
+//        $("#state").click(); //Select State
+//        $("#react-select-3-option-0").click(); // Choose State 'NCR'
+//        $("#city").click(); //Select State
+//        $("#react-select-4-option-0").click(); // Choose City 'Delhi'
+        //$("#submit").click(withTimeout(Duration.ofSeconds(1))); // wait 1 sec and submit
+        $("#submit").click(); // submit form
+
+        //Check that the data is saved
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("John"))
+                .shouldHave(text("Malkovich"))
+                .shouldHave(text("JohnMalkovich@gmail.com"))
+                .shouldHave(text("4441122345"))
+                .shouldHave(text("1990"))
+                .shouldHave(text("Maths"))
+                .shouldHave(text("NCR "))
+                .shouldHave(text("Delhi"))
+                .shouldHave(text("Sports"))
+                .shouldHave(text("Male"));
+    }
+}
+
