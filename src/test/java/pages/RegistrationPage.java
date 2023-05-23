570599c9d395b9bb04f5f3b5ef9f18523976e2ce
Крылович Сергей Станиславович <siarhei.krylovich@softclub.by>
2023-05-23 13:55:29 +0300

    add new test methods for practice form

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -3,6 +3,11 @@ package pages;
 import pages.components.CalendarComponent;
 import pages.components.RegistrationResultModal;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -11,10 +16,10 @@ import static com.codeborne.selenide.Selenide.executeJavaScript;
 public class RegistrationPage {
     CalendarComponent calendarComponent = new CalendarComponent();
     RegistrationResultModal registrationResultModal = new RegistrationResultModal();
-    private static String urnForm = "/automation-practice-form";
+    private static String urlForm = "/automation-practice-form";
 
     public RegistrationPage openPage() {
-        open(urnForm);
+        open(urlForm);
         $("div.main-header").shouldHave(text("Practice Form"));
         //Remove footer, so that it does not interfere with pressing submit button
         executeJavaScript("$('footer').remove()");
@@ -60,10 +65,76 @@ public class RegistrationPage {
         return this;
     }
 
-    public RegistrationPage verifyModalResults () {
-        registrationResultModal.verifyModalAppear();
-
+    public RegistrationPage setSubject (String subject) {
+        //add data to the 'Subjects' field
+        $("#subjectsInput").setValue(subject).pressEnter();
 
         return this;
     }
+
+    public RegistrationPage setHobbies (String hobbie) {
+        //select all checkboxes of the 'Hobbies' field
+//        HashMap<String, String> mapOfHobbies = new HashMap();
+//        mapOfHobbies.put("Sports", "1");
+//        mapOfHobbies.put("Reading", "2");
+//        mapOfHobbies.put("Music", "3");
+
+        $("[for='hobbies-checkbox-1']").shouldHave(text(hobbie)).click(); // Sports
+//        $("[for='hobbies-checkbox-2']").click(); // Reading
+//        $("[for='hobbies-checkbox-3']").click(); // Music
+
+        return this;
+    }
+
+    public RegistrationPage uploadImg (String path) {
+        //uploading file for picture
+        //File file = new File("src/test/resources/img/img.jpg");
+        //$("#uploadPicture").uploadFile(file);
+        $("#uploadPicture").uploadFromClasspath(path);
+
+        return this;
+    }
+
+    public RegistrationPage setAddress (String address) {
+        $("#currentAddress").setValue(address); // Set Current Address
+
+        return this;
+    }
+
+    public RegistrationPage setCity (String city) {
+        $("#state").click();
+        $("#state").$(byText(city)).click();
+//        $("#city").click(); //Select State
+//        $("#react-select-4-option-0").click(); // Choose City 'Delhi'
+
+        return this;
+    }
+
+    public RegistrationPage setState (String state) {
+        $("#state").click();
+        $("#state").$(byText(state)).click();
+//        $("#state").click(); //Select State
+//        $("#react-select-3-option-0").click(); // Choose State 'NCR'
+
+        return this;
+    }
+
+    public RegistrationPage submitForm () {
+        $("#submit").click(); // submit form
+//       $("#submit").click(withTimeout(Duration.ofSeconds(1))); // wait 1 sec and submit
+        return this;
+    }
+
+    public RegistrationPage verifyModalResults () {
+        registrationResultModal.verifyModalAppear();
+
+        return this;
+    }
+
+    public RegistrationPage verifyResults (String key, String value) {
+        registrationResultModal.verifyResultsInModal(key, value);
+
+        return this;
+    }
+
 }
--- src/test/java/pages/components/RegistrationResultModal.java
+++ src/test/java/pages/components/RegistrationResultModal.java
@@ -1,12 +1,21 @@
 package pages.components;
 
+import pages.RegistrationPage;
+
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
 public class RegistrationResultModal {
     public void verifyModalAppear() {
         $(".modal-dialog").should(appear);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+    }
+
+    public void verifyResultsInModal (String key, String value) {
+        $(".table-responsive").$(byText(key))
+                .parent()
+                .shouldHave(text(value));
     }
 }
